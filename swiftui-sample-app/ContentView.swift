69e4277a06491cdfbc51609ae8de13c21f16e040
jkkitakita <jkkitakita@gmail.com>
2020-03-13 18:26:59 +0900

    Initial Commit

---
+++ swiftui-sample-app/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  swiftui-sample-app
+//
+//  Created by Jun Kitamura on 2020/03/13.
+//  Copyright © 2020 jkkitakita. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ swiftui-sample-appTests/swiftui_sample_appTests.swift
@@ -0,0 +1,34 @@
+//
+//  swiftui_sample_appTests.swift
+//  swiftui-sample-appTests
+//
+//  Created by Jun Kitamura on 2020/03/13.
+//  Copyright © 2020 jkkitakita. All rights reserved.
+//
+
+import XCTest
+@testable import swiftui_sample_app
+
+class swiftui_sample_appTests: XCTestCase {
+
+    override func setUp() {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
